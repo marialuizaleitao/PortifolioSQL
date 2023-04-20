c0373e98dd198f2d09e88473f419698a71d1fde6
Maria Luiza Leitão <88951059+marialuizaleitao@users.noreply.github.com>
2023-04-19 21:19:28 -0300

    Update SQLscript.sql

--- Exercício 01 - Livraria/SQLscript.sql
+++ Exercício 01 - Livraria/SQLscript.sql
@@ -21,11 +21,11 @@ INSERT INTO LIVROS VALUES ('Cavaleiro Real','Ana Claudia','F',465,'Atlas',49.9,'
                           ('SQL para leigos','João Nunes','M',450,'Addison',98,'SP',2018),
                           ('Receitas Caseiras','Celia Tavares','F',210,'Atlas',45,'RJ',2008),	
                           ('Pessoas Efetivas','Eduardo Santos','M',390,'Beta',78.99,'RJ',2018),
-						  ('Habitos Saudaveis','Eduardo Santos','M',630,'Beta',150.98,'RJ',2019),	
+			  ('Habitos Saudaveis','Eduardo Santos','M',630,'Beta',150.98,'RJ',2019),	
                           ('A Casa Marrom','Hermes Macedo','M',250,'Bubba',60,'MG',2016),
-						  ('Estacio Querido','Geraldo Francisco','M',310,'Insignia',100,'ES',2015),	
-                          ('Pre sempre amigas','Leda Silva','F',510,'Insignia',78.98,'ES',2011),
-						  ('Copas Inesqueciveis','Marco Alcantara','M',200,'Larson',130.98,'RS',2018),	
+			  ('Estacio Querido','Geraldo Francisco','M',310,'Insignia',100,'ES',2015),	
+                          ('Pre sempre amigas','Leda Silva','F',510,'Insignia',78.98,'ES',2011)
+			  ('Copas Inesqueciveis','Marco Alcantara','M',200,'Larson',130.98,'RS',2018),	
                           ('O poder da mente','Clara Mafra','F',120,'Continental',56.58,'SP',2017);
                           
 /*PROJEÇÃO DOS DADOS*/                         
